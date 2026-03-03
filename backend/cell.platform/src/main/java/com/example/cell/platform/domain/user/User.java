05cbde153c765119847fa92e4885fb4462827ff7
sinsohi <sohi8451@naver.com>
2026-03-03 20:43:12 +0900

    feat: User 도메인 내용 유효성 검증 구현

--- backend/cell.platform/src/main/java/com/example/cell/platform/domain/user/User.java
+++ backend/cell.platform/src/main/java/com/example/cell/platform/domain/user/User.java
@@ -1,5 +1,7 @@
 package com.example.cell.platform.domain.user;
 
+import com.example.cell.platform.exception.BadRequestException;
+import com.example.cell.platform.exception.ErrorCode;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -14,8 +16,14 @@ public class User {
     private Role role;
     private LocalDateTime createdAt;
 
+    private static final String ERROR_NAME_IS_BLANK = "이름은 빈칸 또는 공백일 수 없습니다.";
+    private static final String ERROR_PASSWORD_IS_BLANK = "비밀번호는 빈칸 또는 공백일 수 없습니다.";
+
     @Builder
     public User(Long id, String username, String password, Role role, LocalDateTime createdAt) {
+        validateUsername(username);
+        validatePassword(password);
+
         this.id = id;
         this.username = username;
         this.password = password;
@@ -29,5 +37,17 @@ public class User {
                 .password(password)
                 .role(role)
                 .build();
+    }
+
+    private void validateUsername(String username) {
+        if (username == null || username.isBlank()) {
+            throw new BadRequestException(ERROR_NAME_IS_BLANK, ErrorCode.U003);
+        }
+    }
+
+    private void validatePassword(String password) {
+        if (password == null || password.isBlank()) {
+            throw new BadRequestException(ERROR_PASSWORD_IS_BLANK, ErrorCode.U004);
+        }
     }
 }
