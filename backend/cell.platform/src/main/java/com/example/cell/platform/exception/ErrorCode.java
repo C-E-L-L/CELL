0064786592a75bfc4f6940b4a93c8476d9e78cfa
sinsohi <sohi8451@naver.com>
2026-03-03 20:10:50 +0900

    feat: User 관련 에러 코드 추가

--- backend/cell.platform/src/main/java/com/example/cell/platform/exception/ErrorCode.java
+++ backend/cell.platform/src/main/java/com/example/cell/platform/exception/ErrorCode.java
@@ -2,6 +2,11 @@ package com.example.cell.platform.exception;
 
 public enum ErrorCode {
 
+    // User 관련
+    U000("회원가입 시, 이미 존재하는 아이디로 가입을 시도한 경우"),
+    U001("로그인 시, 아이디 또는 비밀번호가 일치하지 않는 경우"),
+    U002("회원가입 시, 유효하지 않은 역할(Role)을 입력한 경우"),
+
     // Global
     G000("필수 필드의 값이 NULL 또는 빈 값인 경우"),
     G001("파일 처리 실패"),
