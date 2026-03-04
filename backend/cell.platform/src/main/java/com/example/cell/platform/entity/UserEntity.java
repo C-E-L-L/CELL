dedaf9138fe045a6866ab0db99108ab2fa02a618
sinsohi <sohi8451@naver.com>
2026-03-04 17:19:08 +0900

    fix: BaseEntity 상속하도록 수정

--- backend/cell.platform/src/main/java/com/example/cell/platform/entity/UserEntity.java
+++ backend/cell.platform/src/main/java/com/example/cell/platform/entity/UserEntity.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "users")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class UserEntity {
+public class UserEntity extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
