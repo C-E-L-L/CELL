65b59e1e23e5753e9389ea62c412d575c6f41f35
sinsohi <sohi8451@naver.com>
2026-03-04 15:40:55 +0900

    test: TaskTest 작성

---
+++ backend/cell.platform/src/test/java/com/example/cell/platform/domain/TaskTest.java
@@ -0,0 +1,32 @@
+package com.example.cell.platform.domain;
+
+import com.example.cell.platform.domain.task.Task;
+import com.example.cell.platform.domain.task.TaskStatus;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TaskTest {
+
+    @Nested
+    class  Task는 {
+
+        @Test
+        void 정상적인_인자가_들어오면_객체가_생성된다() {
+            // given
+            String originalFilename = "original_sample.jpg";
+            String uploadedFilename = "blood_sample.jpg";
+
+            // when
+            Task task = Task.create(originalFilename, uploadedFilename);
+
+            // then
+            assertThat(task.getOriginalFilename()).isEqualTo(originalFilename);
+            assertThat(task.getUploadedFilename()).isEqualTo(uploadedFilename);
+            assertThat(task.getStatus()).isEqualTo(TaskStatus.IN_PROGRESS);
+            assertThat(task.getId()).isNull();
+            assertThat(task.getCrops()).isEmpty();
+            assertThat(task.getCreatedAt()).isNull();
+        }
+    }
+}
