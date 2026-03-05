3d1b9fd5f23171e7bcb8f33230176d60cbc60b4d
sinsohi <sohi8451@naver.com>
2026-03-05 15:43:12 +0900

    feat: SubmissionEntity 구현

---
+++ backend/cell.platform/src/main/java/com/example/cell/platform/entity/SubmissionEntity.java
@@ -0,0 +1,38 @@
+package com.example.cell.platform.entity;
+
+import com.example.cell.platform.domain.crop.CellType;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "submissions")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SubmissionEntity extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "crop_id", nullable = false)
+    private CropEntity crop;
+
+    @Column(nullable = false)
+    private String studentId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CellType studentLabel;
+
+    @Builder
+    private SubmissionEntity(Long id, CropEntity crop, String studentId, CellType studentLabel) {
+        this.id = id;
+        this.crop = crop;
+        this.studentId = studentId;
+        this.studentLabel = studentLabel;
+    }
+}
